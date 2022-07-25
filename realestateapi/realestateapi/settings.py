3e3c28789069421a11a3e7ce11d094183ef90342
qalux raus <61173724+arthurroffe@users.noreply.github.com>
2022-07-25 16:18:11 +0800

    edit key

--- realestateapi/realestateapi/settings.py
+++ realestateapi/realestateapi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'svs)_mfhjfkpy@1r-fn$=fxtmpjl$7d5ww7d8f(z!@@1t08*s@'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
